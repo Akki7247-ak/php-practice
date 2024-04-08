6d1d253eecdf1d9b4570b0c19543b55f6ceb438c
Akhilesh Prajapati <62496699+Akki7247-ak@users.noreply.github.com>
2024-04-08 14:10:49 +0530

    Add files via upload

---
+++ 01_variable.php
@@ -0,0 +1,23 @@
+<?php 
+
+# variable
+# a container hold data
+
+# data type
+# string intger float boolen
+
+$name = "akhilesh";
+$age = 23;
+$marks = 50.34;
+$married = false;
+
+
+echo "my name is ". $name ."<br>";
+echo "my age is ". $age ."<br>";
+echo "my marks is ". $marks ."<br>";
+echo "my married is ". $married;
+
+var_dump($name);
+var_dump($age);
+var_dump($marks);
+var_dump($married);
---
+++ 02_airthmetics_operator.php
@@ -0,0 +1,37 @@
+<?php 
+#airthmetics opertors
+
+// $a = 10;
+// $b = 4;
+
+// $c = $a + $b;
+// $c = $a - $b;
+// $c = $a * $b;
+// $c = $a / $b;
+// $c = $a % $b;
+// $c = $a ** $b;
+// echo $c;
+
+# incriment and decriment 
+
+
+// $a = 11;
+// $b = $a++;
+// $b = ++$a;
+
+// $b = $a--;
+// $b = --$a;
+// echo $a;
+
+
+#opertors precendence
+
+//  ()
+//  **
+//  * / %
+//  +-
+
+// $total = 2 * 3 + (2 + 2) ** 5;
+//2 * 3 + (25) * 5
+//6 + 125;
+// echo $total;
