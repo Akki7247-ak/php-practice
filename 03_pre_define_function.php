016aad38ef87a0551166daf4b081f6afc5862718
Akhilesh Prajapati <62496699+Akki7247-ak@users.noreply.github.com>
2024-04-09 00:02:47 +0530

    Add files via upload

---
+++ 03_pre_define_function.php
@@ -0,0 +1,69 @@
+<?php 
+
+/*
+pre define function
+
+string function
+number function
+math function
+
+*/
+
+// $name = "akhilesh";
+
+// echo strtoupper($name);
+// echo "<br>";
+
+// echo strtolower("AKHILESH");
+// echo "<br>";
+
+// echo ucfirst("akhilesh");
+// echo "<br>";
+
+// echo ucwords("this is example of first word");
+// echo "<br>";
+
+// echo strlen("what is legth");
+// echo "<br>";
+
+// echo trim("this is   unc de  fjnds");
+// echo "<br>";
+
+// echo ltrim("           dcsdmolcv vfvsf vfsvsf vsfdv vfdsv ");
+// echo "<br>";
+
+// echo rtrim("dcsdmolcv vfvsf vfsvsf     vsfdv         vfdsv                      ");
+// echo "<br>";
+
+// echo sprintf("%a", $a);
+// echo str_word_count("how much words in this line");
+// echo "<br>";
+
+// $a="this is something i dont know what is this";
+// $count = str_word_count($a,1);
+
+// $name = "skhilesh";
+// echo str_replace("s", "a", $name);
+// echo "<br>";
+
+// $name = "Akhilesh";
+// echo str_ireplace("a", "Akkkk", $name);
+
+
+# number function
+
+// $num = 30;
+// var_dump(is_int($num));
+// var_dump(is_float($num));
+// var_dump(is_numeric($num));
+
+
+
+# math function
+
+echo pi()."<br>";
+echo abs(-11)."<br>";
+echo round(30.50)."<br>";
+echo sqrt(100)."<br>";
+echo rand()."<br>";
+echo rand(1, 100)."<br>";
