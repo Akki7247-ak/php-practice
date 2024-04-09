aeb56070ac9ee93743a2f37b861847992dd1200c
Akhilesh Prajapati <62496699+Akki7247-ak@users.noreply.github.com>
2024-04-09 18:21:04 +0530

    learning if elseif else condition how is it work

---
+++ 04_if_condition.php
@@ -0,0 +1,34 @@
+<?php
+# if condition statement 
+# if value is true, do something
+# if value false, do nothing 
+
+# relational operator
+# > greater then 
+# < less then
+# >= greater then or equal to
+# <= less then or equal to
+# ! not
+# == equal to equal to
+
+// $age = 20;
+
+// if($age >= 21){
+//     echo "you can vote";
+// }else{
+//     echo "you can not vote";
+// }
+
+
+// $hours = 50;
+// $rate = 50;
+// $weeklyPay = 0;
+// if ($hours <= 40) {
+//     $weeklyPay = $hours * $rate;
+// } elseif ($hours <= 0) {
+//     $weeklyPay = 0;
+// } else{
+//     $weeklyPay = (40 * $rate) + (($hours - 40) * $rate);
+// }
+
+// echo "your weekly payment  is : " . $weeklyPay;
